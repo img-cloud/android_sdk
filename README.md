0b4435ef02d8b757625b97556eb3712cfd95e0db
Chandan Kumar <chandankumar.r@gmail.com>
2015-09-02 03:05:13 +0530

    Refactor the code
    * Remove dependency on android libarary
    * Gradle can generate jar file that can be directly imported in the project
    * Refactored method signatures

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # android_sdk
-Android Client for using Image upload &amp; transformation APIs for img-cloud
 
-Img-cloud library which offers url after uploading images.
+Java Client for using Image upload &amp; transformation APIs for img-cloud.
+
+This client has dependency on Retrofit and can be used in Android
+
+Img-cloud library which offers URL after uploading images.
 
 <b>Configuration</b>
 Each request for building a URL must have api_key which will get after signing up <a href="http://img-cloud.herokuapp.com/">Here</a> .
@@ -10,15 +13,23 @@ Each request for building a URL must have api_key which will get after signing u
 
 Here's an example to configure 
 
-            UploadImageAPI imageAPI = = new UploadImageAPI(yourApiKey);
-            try {
-                    //call api to upload the image
-                    Map imgResponse = imageAPI.upload(imagePath, folderName,tags);
-                    return imgResponse;
-                } catch (RetrofitError error) {
-                       //handle the error
-                   return imgResponse;
-                }
-        
+    ImgCloud.setApiKey(YOUR_API_KEY);
+    ImgCloud imgUpload = new ImgCloud();
+    imgUpload.upload("/path/to/photo.jpg", "folderName", "tag1, tag2");
+    String uploadedUrl = imgUpload.uploadedUrl();
+    String resizedUrl = imgUpload.getResizedUrl(200, 200);
+
+    String resizedUrlFromExistingUrl = ImgCloud.resizedUrl(remoteUrl, 400, 400);
+
+##  Compiling the source
+
+To clean
+    
+    gradle clean
+
+To create jar
+
+    gradle jar
+
 <B>License</B>
 Under Liftoffllc license 
--- imgcloudlib/src/main/java/com/liftoffllc/imgcloudlib/ImgCloud.java
+++ imgcloudlib/src/main/java/com/liftoffllc/imgcloudlib/ImgCloud.java
@@ -1,7 +1,5 @@
 package com.liftoffllc.imgcloudlib;
 
-import android.util.Log;
-
 import com.liftoffllc.imgcloudlib.interfaces.ApiService;
 
 import java.io.File;
@@ -12,66 +10,113 @@ import retrofit.mime.TypedFile;
 import retrofit.mime.TypedString;
 
 /**
+ * To set API Key
+ * <code>
+ * ImgCloud.setApiKey("YOUR API KEY");
+ * </code>
+ *
+ * <p>
+ * Above needs to be done once in entire life cycle of your app.
+ * Unless you want to intentionally want to change the API Key.
+ * </p> 
+ * 
+ * <code>
+ * ImgCloud uploadImg = new ImgCloud();
+ * uploadImg.upload("path/to/your/file", "android1", "android1");
+ * </code>
+ * To get URL of uploaded image
+ * <code>
+ * uploadImg.uploadedUrl();
+ * </code>
+ * 
  * Created by Nikita on 31/08/15.
  */
+
 public class ImgCloud {
 
+	private static String apiKey;
+	private String serviceUrl = "http://img-cloud.liftoffllc.in";
+	private ApiService imageService = null;
+	private RestAdapter restAdapter = null;
+	private Map uploadMap = null;
 
+	public ImgCloud() {
+		getService();
+	}
 
-    private static String apiKey;
+	public ImgCloud(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+		getService();
+	}
 
-    private String serviceUrl = "http://img-cloud.liftoffllc.in";
+	/**
+	 * Use the overloaded method which returns ImgCloud object
+	 * @param image
+	 * @return
+	 */
+	@Deprecated
+	public Map upload(String image) {
+		TypedFile tFile = new TypedFile("image", new File(image));
+		return imageService.uploadImage(tFile, new TypedString(getApiKey()),
+				new TypedString(null), new TypedString(null));
+	}
+	
+	/**
+	 * Usage
+	 * 
+	 * @param image absolute path of the file
+	 * @param folderName the folder location where image needs to be stored. If this is null, 
+	 * it should default.  
+	 * @param tags comma separated string of tags
+	 * @return
+	 * imgCloud object on successful upload
+	 */
+	public ImgCloud upload(String image, String folderName, String tags) {
+		TypedFile tFile = new TypedFile("image", new File(image));
+		this.uploadMap  = imageService.uploadImage(tFile, new TypedString(getApiKey()),
+				new TypedString(folderName), new TypedString(tags));
+		return this;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * remote URL of the file. Should be invoked only if image is already uploaded.
+	 */
+	public String uploadedUrl() {
+		return (String) this.uploadMap.get("url");
+	}
+	
+	/**
+	 * Example Usage
+	 * <code>
+	 * ImgCloud.resizedUrl(currentPost.getMediaUrl(), 400, 400);
+	 * </code>
+	 * @param width
+	 * @param height
+	 * @return
+	 * remote URL of given dimension
+	 * 
+	 */
+	public String getResizedUrl(int width, int height) {
+		return Util.getTransformedUrl((String)this.uploadMap.get("url"), width, height);
+	}
+	
+	public static String resizedUrl(String url, int w, int h) {
+		return Util.getTransformedUrl(url, w, h);
+	}
 
-    private ApiService imageService = null;
-    private RestAdapter restAdapter = null;
-    public ImgCloud(){
-        getService();
+	public static String getApiKey() {
+		return apiKey;
+	}
 
-    }
-
-    public ImgCloud(String serviceUrl){
-
-
-        this.serviceUrl = serviceUrl;
-        getService();
-    }
-
-    public Map upload(String image){
-
-        TypedFile tFile = new TypedFile("image", new File(image));
-        return imageService.uploadImage(tFile, new TypedString(getApiKey()), new TypedString(null), new TypedString(null));
-    }
-
-
-    public Map upload(String image,String folderName, String tags){
-
-        TypedFile tFile = new TypedFile("image", new File(image));
-        return imageService.uploadImage(tFile, new TypedString(getApiKey()), new TypedString(folderName), new TypedString(tags));
-    }
-
-
-    private ApiService getService() {
-        restAdapter = new RestAdapter.Builder()
-                .setEndpoint(serviceUrl)
-                .build();
-        imageService = restAdapter.create(ApiService.class);
-        return imageService;
-    }
-
-    public static String getResizedImg(String url,int w,int h){
-
-        String[] splited = url.split("/");
-        String resizedUrl = url.substring(0, url.lastIndexOf("/")) + "/h_" + h + "," + w + "/" + splited[splited.length - 1];
-        
-        return resizedUrl;
-        }
-
-
-    public static String getApiKey() {
-        return apiKey;
-    }
-
-    public static void setApiKey(String apiKey) {
-        ImgCloud.apiKey = apiKey;
-    }
+	public static void setApiKey(String apiKey) {
+		ImgCloud.apiKey = apiKey;
+	}
+	
+	private ApiService getService() {
+		restAdapter = new RestAdapter.Builder().setEndpoint(serviceUrl).build();
+		imageService = restAdapter.create(ApiService.class);
+		return imageService;
+	}
 }
